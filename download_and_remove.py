0b5a5d5017b73f50c1b16b638fef8daaf2377451
AON25650572 <64964211+AON25650572@users.noreply.github.com>
2021-01-25 21:40:26 +0900

    Update download_and_remove.py

--- download_and_remove.py
+++ download_and_remove.py
@@ -1,7 +1,7 @@
 import updown
 
-TOKEN = 'fzGKAI2TU6wAAAAAAAAAAQjP2w9F9mD6GSFa0sTRzQNuId_TT3q37p9AvhBHZs3L'
-download_folder = 'C:\\Users\\aon25\\Documents\\GitHub\\Raspberry_Pi_recording_radiko'
+TOKEN = 'hogehogehoge'
+download_folder = 'C:\\hogehoge'
 list_folder = updown.list_folder(updown.dropbox.Dropbox(TOKEN),'','')
 
 for fname in list_folder.keys():

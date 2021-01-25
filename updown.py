ef8e7276d557755591a29edb7e4c559d3d6c4af3
AON25650572 <64964211+AON25650572@users.noreply.github.com>
2021-01-25 21:41:56 +0900

    Update updown.py

--- updown.py
+++ updown.py
@@ -20,7 +20,7 @@ if sys.version.startswith('2'):
 import dropbox
 
 # OAuth2 access token.  TODO: login etc.
-TOKEN = 'fzGKAI2TU6wAAAAAAAAAAQjP2w9F9mD6GSFa0sTRzQNuId_TT3q37p9AvhBHZs3L'
+TOKEN = 'hogehogehoge'
 
 parser = argparse.ArgumentParser(description='Sync ~/Downloads to Dropbox')
 parser.add_argument('folder', nargs='?', default='Downloads',

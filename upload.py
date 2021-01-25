cdbc0856e219062830448f638c164f994a480bed
AON25650572 <64964211+AON25650572@users.noreply.github.com>
2021-01-25 21:47:44 +0900

    Update upload.py

--- upload.py
+++ upload.py
@@ -4,7 +4,7 @@ import sys
 args = sys.argv
 folder_path = args[1]
 fname = args[2]
-TOKEN = 'fzGKAI2TU6wAAAAAAAAAAQjP2w9F9mD6GSFa0sTRzQNuId_TT3q37p9AvhBHZs3L'
+TOKEN = 'hogehogehoge'
 
 
 updown.upload(updown.dropbox.Dropbox(TOKEN),folder_path+'/'+fname,'','',fname)
